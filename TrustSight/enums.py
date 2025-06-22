5f3ae54a474b64f1a2de7cd0d2bf176a8d080c77
Atharv Warade <atharvwarade@gmail.com>
2025-06-22 23:37:01 +0530

    Deleted everythign

--- TrustSight/enums.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class Priority(Enum):
-    """Priority levels for fraud detection request processing"""
-    CRITICAL = 1
-    HIGH = 2
-    MEDIUM = 3
-    LOW = 4
-    SKIP = 5
